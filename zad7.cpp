57a8f58234ca7c4d400fa89407bc0ddbadff9a79
Piotr Kociolek <piotrkociolek2002@gmail.com>
2022-11-13 22:33:08 +0100

    wer 2.5

---
+++ zad7.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float a,b,c,d,x,x1,x2,p,q; 
+void kanoniczna ()
+{
+	b= -2*a*p;
+	c= a*(p*p)+q;
+	
+	cout<<"podaj a"<<endl;
+		cin>>a;
+	cout<<"podaj p"<<endl;
+		cin>>p;
+	cout<<"podaj q"<<endl;
+		cin>>q;
+		
+	d=(b*b)-(4*a*c); //delta
+
+    if(d>0)  
+
+    {
+
+        x1=(-b-sqrt(d))/(2*a);  
+
+        x2=(-b+sqrt(d))/(2*a);  
+
+        cout<<"Wynik to :x1 = "<<x1<<"x2 = "<<x2<<endl;
+
+    }
+
+    if(d==0)  
+    {
+		x=-b/(2*a);  
+        cout<<"Wynik to :  x = "<<x<<endl;
+
+    }
+
+    if(d<0)  
+    {      
+    cout<<"Brak rozwi¹zañ"<<endl;  
+    }
+}
+void kwadratowa()
+{
+	cout<<" Podaj wspó³czynnik kierunkowy prostej a "<<endl;;
+    	cin>>a; 
+    cout<<"Podaj wspó³czynnik b "<< endl;
+    	cin>>b;
+    cout<<"Podaj wspó³czynnik c "<<endl;
+    	cin>>c;
+
+    d=(b*b)-(4*a*c); //delta
+
+    if(d>0)  
+
+    {
+
+        x1=(-b-sqrt(d))/(2*a);  
+
+        x2=(-b+sqrt(d))/(2*a);  
+
+        cout<<"Wynik to :x1 = "<<x1<<"x2 = "<<x2<<endl;
+
+    }
+
+    if(d==0)  
+
+    {
+
+        x=-b/(2*a);  
+
+        cout<<"Wynik to :  x = "<<x<<endl;
+
+    }
+
+     if(d<0)  
+
+    {      
+
+    cout<<"Brak rozwi¹zañ"<<endl;  
+    }
+}
+
+void liniowa()
+	{
+	cout << "podaj a" << endl;
+	cin >> a;
+	cout <<"podaj b"<<endl;
+	cin >> b;
+	
+	if(a==0)
+	{
+		cout<<"error"<<endl;	
+	}
+	x=-b/a;
+	cout<<"x = "<<x <<endl;
+	}
+int main()
+{
+    int liczba;
+    cout << "1 jesli liniowa, 2 jesli kwadratowa, 3 jesli kanoniczna: "<<endl;
+    cin >> liczba;
+    if(liczba != 1 && liczba != 2 && liczba !=3)
+    {
+    	cout<<"error liczba z poza zakresu"<<endl;
+	}
+	else
+	{
+	
+	switch( liczba )
+	{
+	case 1:
+	
+		liniowa();
+		break;
+	
+	case 2:
+	
+		kwadratowa();
+		break;
+	case 3:
+		kanoniczna();
+		break;
+	}
+	};
+	return 0;	
+}
